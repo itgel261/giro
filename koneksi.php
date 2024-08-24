ce44033c459fee847eb838589ad0cfb52f2c71d5
IT GEL <it@gel.co.id>
2024-08-24 16:02:50 +0700

    first commit

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "postgresql://postgres.zawbtiiwusvvbwsdngqr:[YOUR-PASSWORD]@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"; // Ganti dengan URL database Anda
     $username = "root"; // Ganti dengan username database Anda
-    $password = "u7ckxlo0E84w8w9t"; // Ganti dengan password database Anda
+    $password = ""; // Ganti dengan password database Anda
     $dbname = "giro"; // Nama database Anda
 
     // Membuat koneksi
